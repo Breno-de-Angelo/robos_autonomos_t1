13d97657d23010fb6841ff90dafe486a4dcc250f
Breno-de-Angelo <brenodeangelo@gmail.com>
2025-02-11 08:51:07 -0300

    Add rviz to launch and static nav2_params

--- src/robo_autonomo/launch/autonomous_movement.launch.py
+++ src/robo_autonomo/launch/autonomous_movement.launch.py
@@ -1,25 +1,46 @@
+import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    turtlebot3_gazebo_launch_dir = FindPackageShare(package='turtlebot3_gazebo').find('turtlebot3_gazebo')
+    this_package_dir = get_package_share_directory('robo_autonomo')
+
+    turtlebot3_gazebo_launch_dir = get_package_share_directory('turtlebot3_gazebo')
     turtlebot3_world_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([turtlebot3_gazebo_launch_dir, '/launch/turtlebot3_world.launch.py'])
     )
-    slam_toolbox_launch_dir = FindPackageShare(package='slam_toolbox').find('slam_toolbox')
+    slam_toolbox_launch_dir = get_package_share_directory('slam_toolbox')
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([slam_toolbox_launch_dir, '/launch/online_async_launch.py']),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
-    nav2_launch_dir = FindPackageShare(package='nav2_bringup').find('nav2_bringup')
+    nav2_launch_dir = get_package_share_directory('nav2_bringup')
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([nav2_launch_dir, '/launch/navigation_launch.py']),
-        launch_arguments={'use_sim_time': 'true'}.items()
+        launch_arguments={'use_sim_time': 'true', 'params_file': f"{this_package_dir}/config/nav2_params.yaml"}.items()
     )
+
+    rviz_config_file = os.path.join(
+        this_package_dir,
+        'config',
+        'teste.rviz'
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
     return LaunchDescription([
         turtlebot3_world_launch,
         slam_toolbox_launch,
         nav2_launch,
+        rviz_node,
     ])
