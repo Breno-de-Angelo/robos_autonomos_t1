35683a909ccd0cf2606d8f27479f5633a1621dcc
Breno-de-Angelo <brenodeangelo@gmail.com>
2025-02-11 08:50:47 -0300

    Move teste.rviz and update gitignore

--- src/robo_autonomo/setup.py
+++ src/robo_autonomo/setup.py
@@ -9,13 +9,14 @@ setup(
     version="0.1.0",
     packages=[package_name],
     data_files=[
-        ("share/" + package_name, ["package.xml"]),  # Include package.xml
-        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),  # Include launch files
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/config", glob("config/*.yaml") + glob("config/*.rviz")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="Your Name",
-    maintainer_email="your.email@example.com",
+    maintainer="Breno de Angelo",
+    maintainer_email="brenodeangelo@gmail.com",
     description="Robo Autonomo package",
     license="MIT",
     tests_require=["pytest"],
